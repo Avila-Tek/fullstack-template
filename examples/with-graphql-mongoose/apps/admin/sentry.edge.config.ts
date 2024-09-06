9984674d7c3f2ed581e066b6ec828223267671a4
lstanislao <stanislaoluis@gmail.com>
2024-09-06 17:09:48 -0400

    chore: cleaning

--- examples/with-graphql-mongoose/apps/admin/sentry.edge.config.ts
+++ examples/with-graphql-mongoose/apps/admin/sentry.edge.config.ts
@@ -3,10 +3,10 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://4575b79cd64f4d94bb0accadf26eb11a@o437709.ingest.us.sentry.io/4504910941585408",
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
--- examples/with-graphql-mongoose/apps/client/sentry.client.config.ts
+++ examples/with-graphql-mongoose/apps/client/sentry.client.config.ts
@@ -2,15 +2,13 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://4575b79cd64f4d94bb0accadf26eb11a@o437709.ingest.us.sentry.io/4504910941585408",
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
