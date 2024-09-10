4010f799478877895912cc21a5d293b8af302949
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2024-09-10 12:06:28 -0400

    chore: :arrow_up: upgrade deps

--- examples/with-graphql-mongoose/apps/client/sentry.edge.config.ts
+++ examples/with-graphql-mongoose/apps/client/sentry.edge.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
