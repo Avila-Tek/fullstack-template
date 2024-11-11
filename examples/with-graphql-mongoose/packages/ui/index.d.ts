9f5a5f436b20e647f2a2b0520fcf9fe95fab5675
Isabel Asenjo <isabel@avilatek.dev>
2024-11-11 15:20:28 -0400

    feat(packages/ui): :lipstick: added ui components to ui package (gql+mongoose)

---
+++ examples/with-graphql-mongoose/packages/ui/index.d.ts
@@ -0,0 +1,2 @@
+declare module '@repo/ui';
+
---
+++ examples/with-graphql-mongoose/packages/ui/src/components/checkbox/CheckboxContext.tsx
@@ -0,0 +1,12 @@
+import { createContext } from 'react';
+
+export interface CheckboxContextValue {
+  selectedItems: string[];
+  toggleItem: (item: string) => void;
+}
+
+const CheckboxContext = createContext<CheckboxContextValue | undefined>(
+  undefined
+);
+
+export default CheckboxContext;
