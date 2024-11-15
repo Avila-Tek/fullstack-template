13fde3f40c9d7d0b6580793aa4337012b31b12c5
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2024-11-15 12:05:54 -0400

    chore(package/ui) refactor the export from package/ui and pin lib versions

--- examples/with-restful-mongoose/apps/client/src/app/page.tsx
+++ examples/with-restful-mongoose/apps/client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import { Button } from '@repo/ui';
+import { Button } from '@repo/ui/atoms';
 
 export default function Home() {
   return (
