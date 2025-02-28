1e0a277097d6abca4a9a9f7931b8220da1286968
Santiago Figueroa <santiagofigueroavasquez@gmail.com>
2025-02-28 00:14:36 -0400

    feat: :sparkles: adding types & install dependencies function

--- examples/with-restful-mongoose/scripts/utils/index.ts
+++ examples/with-restful-mongoose/scripts/utils/index.ts
@@ -1,5 +1,8 @@
 import * as fs from 'fs';
 import { exec } from 'child_process';
+import inquirer from 'inquirer';
+import { Project } from 'ts-morph';
+export * from './file-generator';
 
 /**
  * @function
@@ -32,8 +35,6 @@ export function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export * from './file-generator';
-
 export function formatFiles(files: string[]): void {
   const command = `npx prettier --write ${files.join(' ')} --single-quote`;
 
@@ -48,4 +49,91 @@ export function formatFiles(files: string[]): void {
     }
     console.log(stdout);
   });
+}
+
+interface IDependency {
+  name: string;
+  dev: boolean;
+  peer?: boolean;
+}
+
+export function installDependencies(
+  workspace: string,
+  dependencies: IDependency[]
+): void {
+  const command = `npm install -S --workspace ${workspace} ${dependencies
+    .map(
+      (dep) =>
+        `${dep.name}${dep.dev ? ' -D' : ''}${dep.peer ? ' --save-peer' : ''}`
+    )
+    .join(' ')}`;
+
+  console.log('Installing Dependencies:', command);
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Install Dependencies stderr: ${stderr}`);
+      return;
+    }
+    console.log(stdout);
+  });
+}
+
+export const architecture = ['RESTful', 'GraphQL'] as const;
+export type Architecture = (typeof architecture)[number];
+
+export const orm = ['Mongoose', 'Prisma'] as const;
+export type Orm = (typeof orm)[number];
+
+export const webService = ['Admin', 'Client', 'Shared'] as const;
+export type WebService = (typeof webService)[number];
+
+export const serverLocation = ['app.ts', 'server.ts'] as const;
+export type ServerLocation = (typeof serverLocation)[number];
+
+export type IAnswer = {
+  backendArchitecture: Architecture;
+  ORM: Orm;
+  webService: WebService;
+  serverLocation: ServerLocation;
+};
+
+export function propmtTechStack() {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'backendArchitecture',
+      message: 'Select backend architecture',
+      choices: architecture,
+    },
+    {
+      type: 'list',
+      name: 'ORM',
+      message: 'Select ORM',
+      choices: orm,
+    },
+    {
+      type: 'list',
+      name: 'webService',
+      message: 'Where do you want the services for the frontend to be?',
+      choices: webService,
+    },
+    {
+      type: 'list',
+      name: 'serverLocation',
+      message: 'Where is your server located?',
+      choices: serverLocation,
+    },
+  ]);
+}
+
+export interface IBoostrap {
+  name: string;
+  project: Project;
+  overwrite: boolean;
+  techStack: IAnswer;
 }
