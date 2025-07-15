23e85f6e6eae96e4e1e7b91994c77b643d573827
SABRINA CORREIA <sabrinacorreia760@gmail.com>
2025-07-15 15:33:09 -0400

    chore: :arrow_up: upgrade all dependencies in restful-mongoose template

---
+++ examples/with-restful-mongoose/apps/admin/src/app/client-providers.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import {
+  type TAnalyticsOption,
+  type TAnalyticsProviderProps,
+} from '@repo/ui/analytics';
+import {
+  type TFeateFlagConfig,
+  type TFeatureFlagContextProviderProps,
+} from '@repo/ui/feature-flags';
+import { ThemeProvider } from 'next-themes';
+
+const FeatureFlagContextProvider = dynamic<TFeatureFlagContextProviderProps>(
+  () =>
+    import('@repo/ui/feature-flags').then(
+      (mod) => mod.FeatureFlagContextProvider
+    ),
+  {
+    ssr: false,
+  }
+);
+
+const AnalyticsProvider = dynamic<TAnalyticsProviderProps>(
+  () => import('@repo/ui/analytics').then((mod) => mod.AnalyticsProvider),
+  {
+    ssr: false,
+  }
+);
+
+interface ClientProvidersProps {
+  children: React.ReactNode;
+  config: TFeateFlagConfig;
+  analyticsOptions: Array<TAnalyticsOption>;
+}
+
+export function ClientProviders({ children, config, analyticsOptions }: ClientProvidersProps) {
+  return (
+    <FeatureFlagContextProvider config={config}>
+      <AnalyticsProvider
+        analyticsAppName="avila-tek-project"
+        analyticsOptions={analyticsOptions}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </AnalyticsProvider>
+    </FeatureFlagContextProvider>
+  );
+}
