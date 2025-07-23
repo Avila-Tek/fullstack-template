43f4259d4e91de527ee3f8d7db91f49847ea2c2c
SABRINA CORREIA <sabrinacorreia760@gmail.com>
2025-07-23 14:16:20 -0400

    feat: :sparkles: swagger config plugin

--- examples/with-restful-mongoose/apps/api/src/app.ts
+++ examples/with-restful-mongoose/apps/api/src/app.ts
@@ -1,72 +1,76 @@
 // Import this first!
-import './instrument';
-import { Server } from 'http';
-import cors from '@fastify/cors';
-import helmet from '@fastify/helmet';
-import rateLimit from '@fastify/rate-limit';
-import * as Sentry from '@sentry/node';
-import Fastify, { FastifyHttpOptions } from 'fastify';
+import "./instrument";
+import type { Server } from "node:http";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import rateLimit from "@fastify/rate-limit";
+import * as Sentry from "@sentry/node";
+import Fastify, { type FastifyHttpOptions } from "fastify";
 import {
-  ZodTypeProvider,
-  serializerCompiler,
-  validatorCompiler,
-} from 'fastify-type-provider-zod';
-import mongoose from 'mongoose';
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import mongoose from "mongoose";
+import { swaggerPlugin } from "./plugins";
 
 export async function createApp() {
-  let connection: typeof mongoose | null = null;
-  try {
-    connection = await mongoose
-      .connect(String(process.env.DATABASE))
-      .then((conn) => {
-        console.log('Connected to database');
-        return conn;
-      });
+	let connection: typeof mongoose | null = null;
+	try {
+		connection = await mongoose
+			.connect(String(process.env.DATABASE))
+			.then((conn) => {
+				console.log("Connected to database");
+				return conn;
+			});
 
-    mongoose.connection.on('error', (err) => `❌🤬❌🤬 ${err}`);
-  } catch (err) {
-    console.log(`ERROR: ${err}`);
-    if (connection && connection.connection) {
-      connection.connection.close();
-    }
-    process.exit(1);
-  }
+		mongoose.connection.on("error", (err) => `❌🤬❌🤬 ${err}`);
+	} catch (err) {
+		console.log(`ERROR: ${err}`);
+		if (connection?.connection) {
+			connection.connection.close();
+		}
+		process.exit(1);
+	}
 
-  let config: FastifyHttpOptions<Server> = {};
+	let config: FastifyHttpOptions<Server> = {};
 
-  if (process.env.NODE_ENV === 'production') {
-    config = {
-      logger: {
-        level: 'info',
-        transport: {
-          target: '@axiomhq/pino',
-          options: {
-            dataset: process.env.AXIOM_DATASET,
-            token: process.env.AXIOM_TOKEN,
-          },
-        },
-      },
-    };
-  }
+	if (process.env.NODE_ENV === "production") {
+		config = {
+			logger: {
+				level: "info",
+				transport: {
+					target: "@axiomhq/pino",
+					options: {
+						dataset: process.env.AXIOM_DATASET,
+						token: process.env.AXIOM_TOKEN,
+					},
+				},
+			},
+		};
+	}
 
-  const app = Fastify(config).withTypeProvider<ZodTypeProvider>();
+	const app = Fastify(config).withTypeProvider<ZodTypeProvider>();
 
-  // Add schema validator and serializer
-  app.setValidatorCompiler(validatorCompiler);
-  app.setSerializerCompiler(serializerCompiler);
+	// Add schema validator and serializer
+	app.setValidatorCompiler(validatorCompiler);
+	app.setSerializerCompiler(serializerCompiler);
 
-  if (process.env.NODE_ENV === 'production') {
-    Sentry.setupFastifyErrorHandler(app);
-    await app.register(helmet);
-    await app.register(rateLimit);
-  }
+	if (process.env.NODE_ENV === "production") {
+		Sentry.setupFastifyErrorHandler(app);
+		await app.register(helmet);
+		await app.register(rateLimit);
+	}
 
-  await app.register(cors, {
-    origin: JSON.parse(process.env.CORS_ORIGINS ?? '["*"]'),
-    credentials: true,
-  });
+	await app.register(cors, {
+		origin: JSON.parse(process.env.CORS_ORIGINS ?? '["*"]'),
+		credentials: true,
+	});
 
-  await app.ready();
+	// Register the swagger plugin
+	app.register(swaggerPlugin);
 
-  return app;
+	await app.ready();
+
+	return app;
 }
