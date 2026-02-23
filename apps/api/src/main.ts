67e2346bc89df92cedc7610c97fc7fdc7ae5134f
Manuel Mendoza <manuelmendozalmeid@gmail.com>
2026-02-23 14:33:47 -0400

    feat(api): :sparkles: helmet, cors and rate limiter added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,36 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 // import { DomainErrorFilter } from './modules/shared/platform/web/DomainErrorFilter';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-	// app.useGlobalFilters(new DomainErrorFilter());
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
 
-	const config = new DocumentBuilder()
-		.setTitle('API')
-		.setDescription('API documentation')
-		.setVersion('1.0')
-		.addBearerAuth()
-		.build();
+  app.enableCors({
+    origin: [process.env.CORS],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api/docs', app, document);
+  // app.useGlobalFilters(new DomainErrorFilter());
 
-	await app.listen(process.env.PORT ?? 8080);
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
